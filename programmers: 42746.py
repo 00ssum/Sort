432a289f2ecf109f81015013fefbc429a4d404d6
조수민 <81895293+00ssum@users.noreply.github.com>
2022-08-19 10:15:17 +0900

    Update programmers: 42746.py

--- programmers: 42746.py
+++ programmers: 42746.py
@@ -11,12 +11,15 @@ def solution(numbers):
 # 참고한 코드
 import functools
 def comparator(a,b):
-    t1 = a+b
-    t2 = b+a
-    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+    #ex> a=10 b=2 => -1 // 6, 10=> 1 // 6,2 =>1
+    t1 = a+b #102
+    t2 = b+a #210
+    # t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+    return (int(t1) > int(t2)) - (int(t1) < int(t2))
 
 def solution(numbers):
-    n = list(map(str,numbers)) #문자열로 변환
-    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
-    answer = str(int(''.join(n)))
+    n=list(map(str,numbers))
+    # cmp_to_key를 기준으로, 내림차순 정렬
+    n = sorted(n, key=functools.cmp_to_key(comparator), reverse=True)
+    answer = str(''.join(n)) #join으로 합쳐서 문자열로 바꾼뒤 출력
     return answer
